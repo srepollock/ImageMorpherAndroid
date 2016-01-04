418083ff45a49e7eb2457f32a51e4203a953d385
Spencer <srepollock@gmail.com>
2016-01-04 14:32:24 -0800

    Working on loading the pictures in

--- app/src/main/java/ca/spollock/morphing/MainActivity.java
+++ app/src/main/java/ca/spollock/morphing/MainActivity.java
@@ -1,6 +1,14 @@
 package ca.spollock.morphing;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -9,7 +17,16 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
+
+    private String selectedImageOne;
+    private String selectedImageTwo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +39,10 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                // action goes here
+                displayTempDialog("Camera not yet ready."); // testing
+                //dispatchTakePictureIntent(); // work on this later
+                //galleryAddPic(); // will then add the photo to the gallery
             }
         });
     }
@@ -42,11 +61,89 @@ public class MainActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        // switch for the item menu selected
+        switch(id){
+            case R.id.action_new:
+                displayTempDialog("new");
+                return true;
 
+            case R.id.action_save:
+                displayTempDialog("saved");
+                return true;
+
+            case R.id.action_load:
+                displayTempDialog("loading");
+                return true;
+
+            case R.id.action_selectImages:
+                // get the picture
+                return true;
+
+            case R.id.action_settings:
+                displayTempDialog("settings");
+                return true;
+
+            // shouldn't hit this
+            default:
+                break;
+        }
         return super.onOptionsItemSelected(item);
     }
+
+    public void displayTempDialog(String Message){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(Message);
+        builder.setCancelable(true);
+        builder.setPositiveButton(R.string.dialog_done, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                dialog.cancel();
+            }
+        });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    String mCurrentPhotoPath;
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        ContextWrapper cw = new ContextWrapper(getApplicationContext());
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = cw.getDir("imageDir", Context.MODE_PRIVATE);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+        return image;
+    }
+
+    static final int REQUEST_TAKE_PHOTO = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                ex.printStackTrace();
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
+                        Uri.fromFile(photoFile));
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+
+
 }
