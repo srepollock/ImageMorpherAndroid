86cb143f1f6c7cb660b4a2bfba7e61722bb1f236
Spencer Pollock <srepollock@gmail.com>
2016-01-11 17:46:46 -0800

    Fixed merge conflicts. Be sure to pull before pushing....

--- app/src/main/java/ca/spollock/morphing/MainActivity.java
+++ app/src/main/java/ca/spollock/morphing/MainActivity.java
@@ -475,9 +475,8 @@ public class MainActivity extends AppCompatActivity
     }
 
     private void removeSpecificLine(int index){
-        FingerLine first = (FingerLine)findViewById(R.id.FirstImageCanvas);
-        FingerLine second = (FingerLine)findViewById(R.id.SecondImageCanvas);
-        first.removeLineNumber(index);
-        second.removeLineNumber(index);
+        lc.remove(index);
+        firstCanvas.removed();
+        secondCanvas.removed();
     }
 }
